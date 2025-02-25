9195a99ea573dbea03aacf3b4f4fb1407584ba21
AvantikaBnsl <148711429+AvantikaBnsl@users.noreply.github.com>
2025-02-25 20:02:05 +0530

    Lab_5 editted

--- distance_classification.py
+++ distance_classification.py
@@ -1,3 +1,13 @@
 import cv2
-cv2.imread("Dr_Shashi_Tharoor.jpg")
-cv2.imread("Plaksha_Faculty.jpg")
+
+def load_data():
+    img1 = cv2.imread("Dr_Shashi_Tharoor.jpg")
+    if img1 is None:
+        return False
+    
+    img2 = cv2.imread("Plaksha_Faculty.jpg")
+    if img2 is None:
+        return False
+    return True
+
+print(load_data())
